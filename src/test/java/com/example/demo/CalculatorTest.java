f547b568f23465f96220d9a4791151119ddb026f
Your Name <you@example.com>
2017-11-10 15:15:25 +0100

    kezdo

---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2,3));
+        assertEquals(8, calculator.sum(4,4));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(8, calculator.sum(16,2));
+        assertEquals(5, calculator.sum(25,5));
+    }
+}
